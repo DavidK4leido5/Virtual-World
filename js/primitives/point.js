d91d66a4679b98f993e5b1289389f17bfec925fc
David <developer.work.david@gmail.com>
2026-08-17 11:31:57 +0800

    added graphEditor with point selector

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -8,11 +8,18 @@ class Point {
     return this.x === point.x && this.y === point.y;
   }
 
-  draw(ctx, size = 18, color = "#000") {
+  draw(ctx, { size = 18, color = "#000", outline = false } = {}) {
     const rad = size / 2;
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.arc(this.x, this.y, rad, 0, 2 * Math.PI);
     ctx.fill();
+    if (outline) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+      ctx.stroke();
+    }
   }
 }
