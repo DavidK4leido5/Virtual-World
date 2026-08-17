6f0eab9b62345bafc2305550bb2b560b3f4b578e
David <developer.work.david@gmail.com>
2026-08-17 12:07:09 +0800

    added preview segments

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -12,12 +12,14 @@ class Segment {
     return this.p1.equals(point) || this.p2.equals(point);
   }
 
-  draw(ctx, width = 2, color = "#000") {
+  draw(ctx, { width = 2, color = "#000", dash = [] } = {}) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.strokeStyle = color;
+    ctx.setLineDash(dash);
     ctx.moveTo(this.p1.x, this.p1.y);
     ctx.lineTo(this.p2.x, this.p2.y);
     ctx.stroke();
+    ctx.setLineDash([]);
   }
 }
